d445cf217cdaa8c0f359a74a1f786c990eb6fc76
박성철 <psc106@naver.com>
2018-08-10 18:10:45 +0900

    서버 기본 설정 몇가지 추가(idao, mapper, homecontrol, data...)

--- ClubApplicationServer/src/main/java/com/teamproject/club_application/DB/iDao.java
+++ ClubApplicationServer/src/main/java/com/teamproject/club_application/DB/iDao.java
@@ -1,7 +1,12 @@
 package com.teamproject.club_application.DB;
 
+import java.util.ArrayList;
+
+import com.teamproject.club_application.data.TestData;
+
 //mapper.xml에 작성된 쿼리문과 연결된 메소드들.
 
 public interface iDao {
+	public ArrayList<TestData> getTestData(); 
 
 }
--- ClubApplicationServer/src/main/java/com/teamproject/club_application/HomeController.java
+++ ClubApplicationServer/src/main/java/com/teamproject/club_application/HomeController.java
@@ -1,39 +1,55 @@
 package com.teamproject.club_application;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.teamproject.club_application.DB.iDao;
+import com.teamproject.club_application.data.TestData;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
+	SqlSession sqlSession;
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Autowired
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}	
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	@RequestMapping("/")
+	public String home() {		
 		return "home";
 	}
 	
+	@RequestMapping(value="androidTest.do",produces = "application/json; charset=utf8")
+	@ResponseBody
+	public String testJson(HttpServletRequest request) {
+				
+		iDao iDAO = sqlSession.getMapper(iDao.class);
+		ArrayList<TestData> items = iDAO.getTestData();
+		
+		Gson gson = new Gson();
+		System.out.println("Å×½ºÆ®");		
+		System.out.println(gson.toJson(items).toString());
+		
+		return gson.toJson(items);
+	}
+
+	public String getRandomString(){
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
 }
