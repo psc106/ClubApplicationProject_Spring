b24a72889fce0545bed7555fc8385e339325ee84
박성철 <psc106@naver.com>
2018-08-17 12:59:00 +0900

    모바일 통신 - controller에 추가(mobile/*.do)

--- ClubApplicationServer/src/main/java/com/teamproject/club_application/HomeController.java
+++ ClubApplicationServer/src/main/java/com/teamproject/club_application/HomeController.java
@@ -13,7 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 import com.teamproject.club_application.DB.iDao;
+import com.teamproject.club_application.data.Alarm;
+import com.teamproject.club_application.data.Club;
+import com.teamproject.club_application.data.Comment;
 import com.teamproject.club_application.data.Member;
+import com.teamproject.club_application.data.Post;
+import com.teamproject.club_application.data.Schedule;
 import com.teamproject.club_application.data.TestData;
 
 /**
@@ -50,20 +55,128 @@ public class HomeController {
 		return gson.toJson(items);
 	}
 
-	@RequestMapping(value="mobile/SelectLoginUser.do",produces = "application/json; charset=utf8")
+	@RequestMapping(value="mobile/selectLoginUser.do",produces = "application/json; charset=utf8")
 	@ResponseBody
-	public String selectLoginUserToMobile(HttpServletRequest request) {
+	public String selectLoginUser_toMobile(HttpServletRequest request) {
 		String loginId = request.getParameter("id");
-		String loginPw = request.getParameter("pw");
-				
-		iDao iDAO = sqlSession.getMapper(iDao.class);
-//		Member item = iDAO
-		
+		String loginPw = request.getParameter("pw");		
 		Gson gson = new Gson();
 		
-//		return gson.toJson(items);
-		return null;
+		iDao iDAO = sqlSession.getMapper(iDao.class);
+		Member item = iDAO.selectLoginUser(loginId, loginPw);
+		
+		return gson.toJson(item);
 	}
+
+	@RequestMapping(value="mobile/checkId.do",produces = "application/json; charset=utf8")
+	@ResponseBody
+	public String checkId_toMobile(HttpServletRequest request) {
+		String id = request.getParameter("id");
+		Gson gson = new Gson();
+				
+		iDao iDAO = sqlSession.getMapper(iDao.class);
+		Integer item = iDAO.checkId(id);
+				
+		return gson.toJson(item);
+	}
+
+	@RequestMapping(value="mobile/selectMyAlarm.do",produces = "application/json; charset=utf8")
+	@ResponseBody
+	public String selectMyAlarm_toMobile(HttpServletRequest request) {
+		String userIdStr = request.getParameter("userId");
+		Long userId;
+		Gson gson = new Gson();
+		
+		if(userIdStr!=null) {
+			userId = Long.parseLong(userIdStr);
+		} else {
+			return gson.toJson(-1L);
+		}
+				
+		iDao iDAO = sqlSession.getMapper(iDao.class);
+		ArrayList<Alarm> items = iDAO.selectMyAlarm(userId);
+		
+		return gson.toJson(items);
+	}
+
+	@RequestMapping(value="mobile/selectMyPost.do",produces = "application/json; charset=utf8")
+	@ResponseBody
+	public String selectMyPost_toMobile(HttpServletRequest request) {
+		String userIdStr = request.getParameter("userId");
+		Long userId;
+		Gson gson = new Gson();
+		
+		if(userIdStr!=null) {
+			userId = Long.parseLong(userIdStr);
+		} else {
+			return gson.toJson(-1L);
+		}
+				
+		iDao iDAO = sqlSession.getMapper(iDao.class);
+		ArrayList<Post> items = iDAO.selectMyPost(userId);
+		
+		return gson.toJson(items);
+	}
+
+	@RequestMapping(value="mobile/selectMyComment.do",produces = "application/json; charset=utf8")
+	@ResponseBody
+	public String selectMyComment_toMobile(HttpServletRequest request) {
+		String userIdStr = request.getParameter("userId");
+		Long userId;
+		Gson gson = new Gson();
+		
+		if(userIdStr!=null) {
+			userId = Long.parseLong(userIdStr);
+		} else {
+			return gson.toJson(-1L);
+		}
+				
+		iDao iDAO = sqlSession.getMapper(iDao.class);
+		ArrayList<Comment> items = iDAO.selectMyComment(userId);
+		
+		return gson.toJson(items);
+	}
+
+	@RequestMapping(value="mobile/selectMySchedule.do",produces = "application/json; charset=utf8")
+	@ResponseBody
+	public String selectMySchedule_toMobile(HttpServletRequest request) {
+		String userIdStr = request.getParameter("userId");
+		Long userId;
+		Gson gson = new Gson();
+		
+		if(userIdStr!=null) {
+			userId = Long.parseLong(userIdStr);
+		} else {
+			return gson.toJson(-1L);
+		}
+				
+		iDao iDAO = sqlSession.getMapper(iDao.class);
+		ArrayList<Schedule> items = iDAO.selectMySchedule(userId);
+		
+		return gson.toJson(items);
+	}
+
+	@RequestMapping(value="mobile/selectMyGroup.do",produces = "application/json; charset=utf8")
+	@ResponseBody
+	public String selectMyGroup_toMobile(HttpServletRequest request) {
+		String userIdStr = request.getParameter("userId");
+		Long userId;
+		Gson gson = new Gson();
+		
+		if(userIdStr!=null) {
+			userId = Long.parseLong(userIdStr);
+		} else {
+			return gson.toJson(-1L);
+		}
+				
+		iDao iDAO = sqlSession.getMapper(iDao.class);
+		ArrayList<Club> items = iDAO.selectMyClub(userId);
+		
+		return gson.toJson(items);
+	}
+	
+	
+	
 	
 	public String getRandomString(){
         return UUID.randomUUID().toString().replaceAll("-", "");
